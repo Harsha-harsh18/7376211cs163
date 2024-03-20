dc130b4cbdff463ecedce7dc0f5c69774218b755
Harsha-harsh18 <99941659+Harsha-harsh18@users.noreply.github.com>
2024-03-20 11:56:39 +0530

    Update dbConnection.cjs

--- AFFORDMED_BACKEND/dbConnection.cjs
+++ AFFORDMED_BACKEND/dbConnection.cjs
@@ -1,1 +1,22 @@
+//code for connection with mongodb
+
+const {MongoClient} = require('mongodb')
+
+let dbConnection
+function connectToDb(callBack) {
+    dbConnection =MongoClient.connect('mongodb://127.0.0.1:27017/RecipeSharingApp').then(function(client) { // this 127.0.0..(go live and get this and paste)
+        dbConnection = client.db()
+        console.log('connected')
+        callBack()
+    }).catch(function(error) {
+        callBack(error)
+    })
+}
+
+function getDb() {
+    return dbConnection
+}
+
+// Exporting required functions
+module.exports = {connectToDb, getDb}
 
